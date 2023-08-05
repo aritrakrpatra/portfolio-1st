af70b57ed3ed5ffb2188f65538c58e2270e6476e
aritrakrpatra <108139687+aritrakrpatra@users.noreply.github.com>
2023-08-06 00:02:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
